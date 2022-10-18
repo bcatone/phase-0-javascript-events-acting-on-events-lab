d08463fa413264cca6b73a5e2de71ae0a64a7e9b
Brittany Laura Catone <bcatone@outlook.com>
2022-10-17 23:52:06 -0400

    commit tree a0a3c6adda3839bbda3a2fdd37bc7eefccd04300

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+// Set the speed of the dodger
+const dodgerSpeed = 1;
+
+// Get the left position of the dodger
+function getDodgerLeft() {
+    const leftNumbers = dodger.style.left;
+    const left = parseInt(leftNumbers, 10);
+    return left;
+}
+
+function moveDodgerLeft() {
+    const left = getDodgerLeft();
+    
+    if (left > 0) {
+     dodger.style.left = `${left - dodgerSpeed}px`;
+    }
+
+    const x = getDodgerLeft();
+    console.log(`New Position: ${x}`);
+}
+
+function moveDodgerRight() {
+    const left = getDodgerLeft();
+    const width = 40;
+    const gameWidth = 400;
+    const maxLeft = gameWidth - width - dodgerSpeed;
+  
+  if (left <= maxLeft) {
+    dodger.style.left = `${left + dodgerSpeed}px`;
+  }
+
+  const x = getDodgerLeft();
+  console.log(`New Position: ${x}`);
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
